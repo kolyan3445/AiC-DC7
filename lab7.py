a5e87efb725c1644b74dc5f1cd8659d608c13f2b
kolyan3445 <55030059+kolyan3445@users.noreply.github.com>
2023-11-14 01:09:43 +0400

    Add files via upload

--- lab7.py
+++ lab7.py
@@ -1,39 +1,63 @@
-#Вариант 14. Конвейер сборки состоит из 10 технологических мест. На 4 из них требуется силовая подготовка (мужчины).
-#Конвейер должен работать в 2 смены. Сформировать все возможные варианты рабочего расписания, если в цехе работает 20 рабочих: 12 женщин и 8 мужчин.
-#TODO: Новая техника - меньше требуется в смену - не требуется силовая подготовка
-#Новая техника увеличила эффективность в 2 раза, потому требования по местам снижены в 2 раза, как и требования по физ. подготовке
+# Требуется для своего варианта второй части л.р. №6 (усложненной программы) написать объектно-ориентированную реализацию.
+# В программе должны быть реализованы минимум один класс, три атрибута, два метода
+
+
+# Вариант 14. Конвейер сборки состоит из 10 технологических мест. На 4 из них требуется силовая подготовка (мужчины).
+# Конвейер должен работать в 2 смены. Сформировать все возможные варианты рабочего расписания, если в цехе работает 20 рабочих: 12 женщин и 8 мужчин.
+# "Часть 2:
+# На заводе заметили, что разговоры между мужчинами занимают рабочее время,"
+# "потому было решено ввести правило: мужчины на смене не могут стоять рядом на конвейере"
+# "Все возможные смены с четыремя мужчинами и оставшимися женщинами между ними:"
+
 
 class ShiftOMatic:
-    def __init__(self, iterable, r=None):
-        self.pool = tuple(iterable)
-        self.n = len(self.pool)
-        self.r = self.n if r is None else r
-        if self.r > self.n:
-            return
-        self.indices = list(range(self.n))
-        self.cycles = list(range(self.n, self.n - self.r, -1))
+    men = 'M' * 4
+    women = 'W' * 6
+    shifts = []
 
-    def generate_shifts(self):
-        yield tuple(self.pool[i] for i in self.indices[:self.r])
-        while self.n:
-            for i in reversed(range(self.r)):
-                self.cycles[i] -= 1
-                if self.cycles[i] == 0:
-                    self.indices[i:] = self.indices[i + 1:] + self.indices[i:i + 1]
-                    self.cycles[i] = self.n - i
+    def __init__(self, shift):
+        self.shift = shift
+
+    def generate_shifts(self, shift_size_unused=None):
+        pool = tuple(self.shift)
+        shift_size_length = len(pool)
+        shift_size = shift_size_length if shift_size_unused is None else shift_size_unused
+        if shift_size > shift_size_length:
+            return
+        indices = list(range(shift_size_length))
+        cycles_count = list(range(shift_size_length, shift_size_length - shift_size, -1))
+        yield tuple(pool[x] for x in indices[:shift_size])
+        while shift_size_length:
+            for i in reversed(range(shift_size)):
+                cycles_count[i] -= 1
+                if cycles_count[i] == 0:
+                    indices[i:] = indices[i + 1:] + indices[i:i + 1]
+                    cycles_count[i] = shift_size_length - i
                 else:
-                    j = self.cycles[i]
-                    self.indices[i], self.indices[-j] = self.indices[-j], self.indices[i]
-                    yield tuple(self.pool[i] for i in self.indices[:self.r])
+                    j = cycles_count[i]
+                    indices[i], indices[-j] = indices[-j], indices[i]
+                    yield tuple(pool[i] for i in indices[:shift_size])
                     break
             else:
                 return
 
-shift = 'M' * 8 + 'W' * 12
-shift_o_matic = ShiftOMatic(shift, 5)
-shifts = list(set(shift_o_matic.generate_shifts()))
+    def separate_shifts(self):
+        shift_separator = []
+        new_shifts = []
+        shifts = list(set(self.generate_shifts()))
+        for i in range(len(shifts)):
+            shift_separator.append(str(shifts[i]).replace('(', '').replace(')', '').replace("'", "").replace(',', '').replace(' ', ''))
+        for i in shift_separator:
+            if not 'MM' in i:
+                new_shifts.append(i)
+        print("На заводе заметили, что разговоры между мужчинами занимают рабочее время,"
+              " потому было решено ввести правило: мужчины на смене не могут стоять рядом на конвейере\n"
+              " Все возможные смены с четыремя мужчинами и оставшимися женщинами между ними:")
+        for i in range(len(new_shifts)):
+            print('Смена ' + str(i+1) + ': ' + str(new_shifts[i]))
 
-print("Новая техника увеличила эффективность в 2 раза, потому требования по местам снижены в 2 раза, как и требования по физ. подготовке \nРасписание в соответствии с новыми условиями:")
 
-for i in shifts:
-    print(i)
+schedule = ShiftOMatic(getattr(ShiftOMatic, 'men') + getattr(ShiftOMatic, 'women'))
+
+
+ShiftOMatic.separate_shifts(schedule)
